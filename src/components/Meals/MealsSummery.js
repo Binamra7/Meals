2a16363cde65ecaa679ab820c486861ee6991dc3
Binamra7 <binamra7khadka6@gmail.com>
2021-09-06 13:07:52 +0545

    first commit

---
+++ src/components/Meals/MealsSummery.js
@@ -0,0 +1,19 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section>
+      <h1>Delicious Fodd, Delivered To You</h1>
+      <p>
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
