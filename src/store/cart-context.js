c88f01249cedf26b7234b2c64b1f401ad54ee629
Binamra7 <binamra7khadka6@gmail.com>
2022-06-26 12:14:23 +0545

    added addToCart and removeFromCart functions

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const CartContext=React.createContext({
-    items: [],
-    totalAmount: 0,
-    addItem: (item) => { },
-    removeItem: (id) => { }    
-})
+const CartContext = React.createContext({
+	items: [],
+	totalAmount: 0,
+	addItem: (item) => {
+		CartContext.items.push(item);
+		CartContext.totalAmount += item.price;
+	},
+	removeItem: (id) => {
+		CartContext.items = CartContext.items.filter((item) => item.id !== id);
+		CartContext.totalAmount = CartContext.items.reduce(
+			(acc, item) => acc + item.price,
+			0
+		);
+	},
+});
 
 export default CartContext;
